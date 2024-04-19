e44e440596edc9085c53d1b398810916f84e6061
Islomjon <islomjonabduganiyev18482@gmail.com>
2024-04-19 20:17:08 +0500

    Bug fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import SelectCurrency from "../elements/SelectCurrency";
 
 const HeaderWrapper = styled.div`
   width: 100%;
-  padding: 12px 320px;
+  padding: 12px 240px;
   box-shadow: 0px 1px 10px 0px rgba(0, 0, 0, 0.12),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 2px 4px -1px rgba(0, 0, 0, 0.2);
   background: rgba(255, 255, 255, 0);
